acfc49dfc8961cd1a384545c622123bc9bf5e725
cjanarthanangit <janaece_12@rocketmail.com>
2020-03-04 12:13:32 +0530

    Added View Files and Register Form

--- profirst/firstapp/urls.py
+++ profirst/firstapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.register, name="register"),
+    path('registerpath/', views.register, name="registerpath"),
     path('login/', views.loginfirst, name="loginapp"),
+    path('dashboard/', views.dashboard, name="dashboard"),
 ]
--- profirst/firstapp/views.py
+++ profirst/firstapp/views.py
@@ -1,8 +1,47 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 
 def loginfirst(request):
-    return render(request, 'loginfirst.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username='username', password='password')
+        if user is not None:
+            auth.login(request.user)
+            return redirect('dashboard')
+        else:
+            messages.info(request, 'Invalid Credentials')
+            return redirect('login')
+    else:
+        return render(request, 'loginfirst.html')
+
 
 def register(request):
-	return render(request, 'register.html')
+    if request.method == 'POST':
+        fname = request.POST['firstname']
+        lname = request.POST['lastname']
+        uname = request.POST['username']
+        emailid = request.POST['email']
+        ps = request.POST['password']
+        ps1 = request.POST['password1']
+        if ps == ps1:
+            if User.objects.filter(username=uname).exists():
+                messages.info(request, 'Usename Already Exists')
+                return redirect('register')
+            elif User.objects.filter(email=emailid).exists():
+                messages.info(request, 'Email Already Exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(
+                    email=emailid, firstname=fname, lastname=lname, password=ps)
+                user.save()
+                messages.info(request, 'User Created')
+                return redirect('loginfirst')
+    else:
+        return render(request, 'register.html')
+
+
+def dashboard(request):
+    return render(request, 'dash.html')
